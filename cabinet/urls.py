1760844dccd71ede66e3e43de6ae7a51afa415a4
afrolovskiy <frolovskiyav@gmail.com>
2019-04-22 22:33:26 +0300

    Fix GroupLessonCreate view

--- cabinet/urls.py
+++ cabinet/urls.py
@@ -5,6 +5,6 @@ from cabinet import views
 urlpatterns = [
     path('groups/list/', views.GroupList.as_view(), name='group-list'),
     path('groups/create/', views.GroupCreate.as_view(), name='group-create'),
-    path('groups/<pk>/', views.GroupDetail.as_view(), name='group-detail'),
-    path('groups/<pk>/lessons/create/', views.GroupLessonCreate.as_view(), name='lesson-create'),
+    path('groups/<group_id>/', views.GroupDetail.as_view(), name='group-detail'),
+    path('groups/<group_id>/lessons/create/', views.GroupLessonCreate.as_view(), name='lesson-create'),
 ]
--- cabinet/views.py
+++ cabinet/views.py
@@ -23,6 +23,7 @@ class GroupCreate(CreateView):
 class GroupDetail(DetailView):
     model = models.Group
     template_name = 'group_detail.html'
+    pk_url_kwarg = 'group_id'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -30,14 +31,16 @@ class GroupDetail(DetailView):
         context['lessons'] = models.Lesson.objects.filter(group=kwargs['object'])
         return context
 
+
 class GroupLessonCreate(CreateView):
     model = models.Lesson
     form_class = forms.LessonForm
     template_name = 'lesson_create.html'
+    pk_url_kwarg = 'group_id'
 
     def form_valid(self, form):
-        form.instance.group = get_object_or_404(models.Group, pk=self.kwargs['group_id'])
+        form.instance.group = get_object_or_404(models.Group, pk=self.kwargs.get(self.pk_url_kwarg))
         return super(GroupLessonCreate, self).form_valid(form)
 
     def get_success_url(self):
-        return reverse('group-detail', kwargs={'pk': self.object.pk})
+        return reverse('group-detail', kwargs={'group_id': self.object.group_id})
