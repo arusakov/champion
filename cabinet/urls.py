4be577dcc056ee963622c3c65b04805b377e1367
afrolovskiy <frolovskiyav@gmail.com>
2019-05-27 22:33:32 +0300

    request list

--- cabinet/urls.py
+++ cabinet/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', login_required(views.IndexView.as_view()), name='cabinet-index'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='cabinet-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='cabinet-logout'),
+    path('request/list/', login_required(views.RequestList.as_view()), name='cabinet-request-list'),
 ]
--- cabinet/views.py
+++ cabinet/views.py
@@ -1,5 +1,21 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
+
+from promo.models import RequestInfo
 
 
 class IndexView(TemplateView):
     template_name = 'cabinet.html'
+
+
+class RequestList(ListView):
+    model = RequestInfo
+    template_name = 'request-list.html'
+    paginate_by = 3
+
+    def get_ordering(self):
+        if self.request.GET.get('id') == 'desc':
+            return '-id'
+        if self.request.GET.get('id') == 'asc':
+            return 'id'
+
+        return '-id'
