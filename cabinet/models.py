dc7a98efd08725f88474c7b2b9cb2a2b9bf2f839
afrolovskiy <frolovskiyav@gmail.com>
2019-05-28 10:46:16 +0300

    Models for cabinet

--- cabinet/models.py
+++ cabinet/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    key = models.CharField(max_length=32, unique=True)
+
+
+class Trainer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 class Group(models.Model):
@@ -16,3 +26,29 @@ class Group(models.Model):
     )
 
     course = models.CharField(choices=COURSE_CHOICES, max_length=16)
+
+
+class Schedule(models.Model):
+    DAY_MONDAY = 1
+    DAY_TUESDAY = 2
+    DAY_WEDNESDAY = 3
+    DAY_THURSDAY = 4
+    DAY_FRIDAY = 5
+    DAY_SATURDAY = 6
+    DAY_SUNDAY = 7
+    DAY_CHOICES = (
+        (DAY_MONDAY, 'Понедельник'),
+        (DAY_TUESDAY, 'Вторник'),
+        (DAY_WEDNESDAY, 'Среда'),
+        (DAY_THURSDAY, 'Четверг'),
+        (DAY_FRIDAY, 'Пятница'),
+        (DAY_SATURDAY, 'Суббота'),
+        (DAY_SUNDAY, 'Воскресение'),
+    )
+
+    week_day = models.PositiveSmallIntegerField(choices=DAY_CHOICES)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    trainers = models.ManyToManyField(User)
